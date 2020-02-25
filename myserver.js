82c439f2881d084e055cbdb671fad93dd2097392
Doremon1380 <46600952+Doremon1380@users.noreply.github.com>
2020-02-25 23:53:29 +0700

    first commit

---
+++ myserver.js
@@ -0,0 +1,10 @@
+var http=require('http')
+var port = process.env.PORT || 7000;
+
+var server=http.createServer((function(request,response)
+{
+	response.writeHead(200,
+	{"Content-Type" : "text/plain"});
+	response.end("Hello World\n");
+}));
+server.listen(7000);

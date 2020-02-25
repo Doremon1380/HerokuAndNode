0dc0b771b11f6df4706f24ee27641fa5dc7355d2
Doremon1380 <46600952+Doremon1380@users.noreply.github.com>
2020-02-26 00:36:01 +0700

    first commit

--- myserver.js
+++ myserver.js
@@ -1,6 +1,8 @@
 var http=require('http')
 var port = process.env.PORT || 7000;
-
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 var server=http.createServer((function(request,response)
 {
 	response.writeHead(200,
